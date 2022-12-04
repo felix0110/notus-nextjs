95c2de2a0250e2bfa6e534d5ea396f8798f30e27
Felix Wong <felix011018@gmail.com>
2022-12-05 02:01:47 +0800

    updating table

--- components/Cards/CardTable.js
+++ components/Cards/CardTable.js
@@ -1,17 +1,30 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 // components
 
-import TableDropdown from "components/Dropdowns/TableDropdown.js";
+import TableDropdown from 'components/Dropdowns/TableDropdown.js';
 
-export default function CardTable({ color }) {
+export default function CardTable({ color, title, header, body }) {
+  const a = '會員資料';
+  const b = [
+    'UID',
+    '會員名稱',
+    '電郵',
+    'ERC20地址',
+    '資產價值 (USD)',
+    '推薦序號',
+    '推薦人ID',
+    '推薦人數',
+    '註冊時間',
+  ];
+  const c = [];
   return (
     <>
       <div
         className={
-          "relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded " +
-          (color === "light" ? "bg-white" : "bg-blueGray-700 text-white")
+          'relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded ' +
+          (color === 'light' ? 'bg-white' : 'bg-blueGray-700 text-white')
         }
       >
         <div className="rounded-t mb-0 px-4 py-3 border-0">
@@ -19,11 +32,11 @@ export default function CardTable({ color }) {
             <div className="relative w-full px-4 max-w-full flex-grow flex-1">
               <h3
                 className={
-                  "font-semibold text-lg " +
-                  (color === "light" ? "text-blueGray-700" : "text-white")
+                  'font-semibold text-lg ' +
+                  (color === 'light' ? 'text-blueGray-700' : 'text-white')
                 }
               >
-                Card Tables
+                {a}
               </h3>
             </div>
           </div>
@@ -35,64 +48,97 @@ export default function CardTable({ color }) {
               <tr>
                 <th
                   className={
-                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
-                    (color === "light"
-                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
-                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                    'px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left ' +
+                    (color === 'light'
+                      ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
+                      : 'bg-blueGray-600 text-blueGray-200 border-blueGray-500')
                   }
                 >
-                  Project
+                  {b[0]}
                 </th>
                 <th
                   className={
-                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
-                    (color === "light"
-                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
-                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                    'px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left ' +
+                    (color === 'light'
+                      ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
+                      : 'bg-blueGray-600 text-blueGray-200 border-blueGray-500')
                   }
                 >
-                  Budget
+                  {b[1]}
                 </th>
                 <th
                   className={
-                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
-                    (color === "light"
-                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
-                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                    'px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left ' +
+                    (color === 'light'
+                      ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
+                      : 'bg-blueGray-600 text-blueGray-200 border-blueGray-500')
                   }
                 >
-                  Status
+                  {b[2]}
                 </th>
                 <th
                   className={
-                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
-                    (color === "light"
-                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
-                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                    'px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left ' +
+                    (color === 'light'
+                      ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
+                      : 'bg-blueGray-600 text-blueGray-200 border-blueGray-500')
                   }
                 >
-                  Users
+                  {b[3]}
                 </th>
                 <th
                   className={
-                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
-                    (color === "light"
-                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
-                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                    'px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left ' +
+                    (color === 'light'
+                      ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
+                      : 'bg-blueGray-600 text-blueGray-200 border-blueGray-500')
                   }
                 >
-                  Completion
+                  {b[4]}
                 </th>
                 <th
                   className={
-                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
-                    (color === "light"
-                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
-                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                    'px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left ' +
+                    (color === 'light'
+                      ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
+                      : 'bg-blueGray-600 text-blueGray-200 border-blueGray-500')
                   }
-                ></th>
+                >
+                  {b[5]}
+                </th>
+                <th
+                  className={
+                    'px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left ' +
+                    (color === 'light'
+                      ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
+                      : 'bg-blueGray-600 text-blueGray-200 border-blueGray-500')
+                  }
+                >
+                  {b[6]}
+                </th>
+                <th
+                  className={
+                    'px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left ' +
+                    (color === 'light'
+                      ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
+                      : 'bg-blueGray-600 text-blueGray-200 border-blueGray-500')
+                  }
+                >
+                  {b[7]}
+                </th>
+                <th
+                  className={
+                    'px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left ' +
+                    (color === 'light'
+                      ? 'bg-blueGray-50 text-blueGray-500 border-blueGray-100'
+                      : 'bg-blueGray-600 text-blueGray-200 border-blueGray-500')
+                  }
+                >
+                  {b[8]}
+                </th>
               </tr>
             </thead>
+
             <tbody>
               <tr>
                 <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
@@ -100,11 +146,11 @@ export default function CardTable({ color }) {
                     src="/img/bootstrap.jpg"
                     className="h-12 w-12 bg-white rounded-full border"
                     alt="..."
-                  ></img>{" "}
+                  ></img>{' '}
                   <span
                     className={
-                      "ml-3 font-bold " +
-                      +(color === "light" ? "text-blueGray-600" : "text-white")
+                      'ml-3 font-bold ' +
+                      +(color === 'light' ? 'text-blueGray-600' : 'text-white')
                     }
                   >
                     Argon Design System
@@ -146,7 +192,7 @@ export default function CardTable({ color }) {
                     <div className="relative w-full">
                       <div className="overflow-hidden h-2 text-xs flex rounded bg-red-200">
                         <div
-                          style={{ width: "60%" }}
+                          style={{ width: '60%' }}
                           className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-500"
                         ></div>
                       </div>
@@ -163,11 +209,11 @@ export default function CardTable({ color }) {
                     src="/img/angular.jpg"
                     className="h-12 w-12 bg-white rounded-full border"
                     alt="..."
-                  ></img>{" "}
+                  ></img>{' '}
                   <span
                     className={
-                      "ml-3 font-bold " +
-                      +(color === "light" ? "text-blueGray-600" : "text-white")
+                      'ml-3 font-bold ' +
+                      +(color === 'light' ? 'text-blueGray-600' : 'text-white')
                     }
                   >
                     Angular Now UI Kit PRO
@@ -177,7 +223,7 @@ export default function CardTable({ color }) {
                   $1,800 USD
                 </td>
                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <i className="fas fa-circle text-emerald-500 mr-2"></i>{" "}
+                  <i className="fas fa-circle text-emerald-500 mr-2"></i>{' '}
                   completed
                 </td>
                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
@@ -210,7 +256,7 @@ export default function CardTable({ color }) {
                     <div className="relative w-full">
                       <div className="overflow-hidden h-2 text-xs flex rounded bg-emerald-200">
                         <div
-                          style={{ width: "100%" }}
+                          style={{ width: '100%' }}
                           className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-emerald-500"
                         ></div>
                       </div>
@@ -227,11 +273,11 @@ export default function CardTable({ color }) {
                     src="/img/sketch.jpg"
                     className="h-12 w-12 bg-white rounded-full border"
                     alt="..."
-                  ></img>{" "}
+                  ></img>{' '}
                   <span
                     className={
-                      "ml-3 font-bold " +
-                      +(color === "light" ? "text-blueGray-600" : "text-white")
+                      'ml-3 font-bold ' +
+                      +(color === 'light' ? 'text-blueGray-600' : 'text-white')
                     }
                   >
                     Black Dashboard Sketch
@@ -273,7 +319,7 @@ export default function CardTable({ color }) {
                     <div className="relative w-full">
                       <div className="overflow-hidden h-2 text-xs flex rounded bg-red-200">
                         <div
-                          style={{ width: "73%" }}
+                          style={{ width: '73%' }}
                           className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-500"
                         ></div>
                       </div>
@@ -290,11 +336,11 @@ export default function CardTable({ color }) {
                     src="/img/react.jpg"
                     className="h-12 w-12 bg-white rounded-full border"
                     alt="..."
-                  ></img>{" "}
+                  ></img>{' '}
                   <span
                     className={
-                      "ml-3 font-bold " +
-                      +(color === "light" ? "text-blueGray-600" : "text-white")
+                      'ml-3 font-bold ' +
+                      +(color === 'light' ? 'text-blueGray-600' : 'text-white')
                     }
                   >
                     React Material Dashboard
@@ -337,7 +383,7 @@ export default function CardTable({ color }) {
                     <div className="relative w-full">
                       <div className="overflow-hidden h-2 text-xs flex rounded bg-teal-200">
                         <div
-                          style={{ width: "90%" }}
+                          style={{ width: '90%' }}
                           className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-teal-500"
                         ></div>
                       </div>
@@ -354,11 +400,11 @@ export default function CardTable({ color }) {
                     src="/img/vue.jpg"
                     className="h-12 w-12 bg-white rounded-full border"
                     alt="..."
-                  ></img>{" "}
+                  ></img>{' '}
                   <span
                     className={
-                      "ml-3 font-bold " +
-                      +(color === "light" ? "text-blueGray-600" : "text-white")
+                      'ml-3 font-bold ' +
+                      +(color === 'light' ? 'text-blueGray-600' : 'text-white')
                     }
                   >
                     React Material Dashboard
@@ -368,7 +414,7 @@ export default function CardTable({ color }) {
                   $2,200 USD
                 </td>
                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <i className="fas fa-circle text-emerald-500 mr-2"></i>{" "}
+                  <i className="fas fa-circle text-emerald-500 mr-2"></i>{' '}
                   completed
                 </td>
                 <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
@@ -401,7 +447,7 @@ export default function CardTable({ color }) {
                     <div className="relative w-full">
                       <div className="overflow-hidden h-2 text-xs flex rounded bg-emerald-200">
                         <div
-                          style={{ width: "100%" }}
+                          style={{ width: '100%' }}
                           className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-emerald-500"
                         ></div>
                       </div>
@@ -421,9 +467,9 @@ export default function CardTable({ color }) {
 }
 
 CardTable.defaultProps = {
-  color: "light",
+  color: 'light',
 };
 
 CardTable.propTypes = {
-  color: PropTypes.oneOf(["light", "dark"]),
+  color: PropTypes.oneOf(['light', 'dark']),
 };
--- pages/admin/tables.js
+++ pages/admin/tables.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 
 // components
 
-import CardTable from "components/Cards/CardTable.js";
+import CardTable from 'components/Cards/CardTable.js';
 
 // layout for page
 
-import Admin from "layouts/Admin.js";
+import Admin from 'layouts/Admin.js';
 
 export default function Tables() {
   return (
@@ -14,9 +14,6 @@ export default function Tables() {
       <div className="flex flex-wrap mt-4">
         <div className="w-full mb-12 px-4">
           <CardTable />
-        </div>
-        <div className="w-full mb-12 px-4">
-          <CardTable color="dark" />
         </div>
       </div>
     </>
